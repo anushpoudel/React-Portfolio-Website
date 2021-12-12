039100643f083c599b499b6e535c37d3210f230e
anushpoudel <apoudel55@gmail.com>
2021-12-12 17:48:39 +1100

    initial commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,151 @@
+import { motion } from "framer-motion";
+import { lazy, Suspense } from "react";
+import { Link } from "react-router-dom";
+import styled, { keyframes, ThemeProvider } from "styled-components";
+
+import { DarkTheme, mediaQueries } from "./Themes";
+import astronaut from "../assets/Images/spaceman.png";
+import Loading from "../subComponents/Loading";
+//Components
+const SocialIcons = lazy(() => import("../subComponents/SocialIcons"));
+const PowerButton = lazy(() => import("../subComponents/PowerButton"));
+const LogoComponent = lazy(() => import("../subComponents/LogoComponent"));
+const ParticlesComponent = lazy(() =>
+  import("../subComponents/ParticlesComponent")
+);
+const BigTitle = lazy(() => import("../subComponents/BigTitle"));
+
+const Box = styled(motion.div)`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+const float = keyframes`
+0% { transform: translateY(-10px)         }
+    50% { transform: translateY(15px) translateX(15px)        }
+    100% { transform: translateY(-10px)         }
+`;
+
+const SpaceMan = styled(motion.div)`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  animation: ${float} 4s ease infinite;
+  width: 20vw;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+const Main = styled(motion.div)`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 55vw;
+  height: 65vh;
+  z-index: 3;
+  line-height: 1.5;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+  backdrop-filter: blur(4px);
+
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+
+  font-family: "Ubuntu Mono", monospace;
+  font-style: italic;
+
+  ${mediaQueries(40)`
+          width: 60vw;
+          height: 50vh;
+          top:50%;
+          left:50%;
+          transform:translate(-50%,-50%);
+
+
+  `};
+  ${mediaQueries(30)`
+          width: 50vw;
+          height: auto;
+          backdrop-filter: none;
+          margin-top:2rem;
+
+  `};
+
+  ${mediaQueries(20)`
+          padding: 1rem;
+          font-size: calc(0.5rem + 1vw);
+  `};
+`;
+const AboutPage = () => {
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Suspense fallback={<Loading />}>
+        <Box
+          key="skills"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.5 } }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        >
+          <LogoComponent theme="dark" />
+          <PowerButton />
+          <SocialIcons theme="dark" />
+          <ParticlesComponent theme="dark" />
+
+          <SpaceMan
+            initial={{ right: "-20%", top: "100%" }}
+            animate={{
+              right: "5%",
+              top: "10%",
+              transition: { duration: 2, delay: 0.5 },
+            }}
+          >
+            <img src={astronaut} alt="spaceman" />
+          </SpaceMan>
+          <Main
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1, delay: 1 } }}
+          >
+            I'm a software developer and full-time student located in Ballarat,
+            Victoria. I'm a 3x certified Salesforce developer currently working
+            with IBM, building custom scalable CRM solutions for large vendors.
+            I'm also very passionate about Web design and development and have
+            strong critical thinking and programming skills.
+            <br />
+            <br />
+            Certifications:
+            <br />
+            <ul>
+              <li>Platform Developer 1</li>
+              <li>OmniStudio Consultant</li>
+              <li>OmniStudio Developer</li>
+            </ul>
+            <br />
+            If you're interested to know more about me and my work, you can
+            contact me using my social links.&nbsp;
+            <Link
+              style={{ color: "white" }}
+              target="_blank"
+              to="/assets/resume/Anush_Poudel_Resume.pdf"
+            >
+              Download my resume
+            </Link>
+          </Main>
+
+          {/* <a href="/assets/resume/Anush_Poudel_Resume.pdf" download>
+              Download resume
+            </a> */}
+
+          <BigTitle text="ABOUT" top="10%" left="5%" />
+        </Box>
+      </Suspense>
+    </ThemeProvider>
+  );
+};
+
+export default AboutPage;
---
+++ src/components/WorkData.js
@@ -0,0 +1,41 @@
+export const Work = [
+  {
+    id: 1,
+    name: "Photo Gallery",
+    description:
+      "A simple responsive photo gallery app powered by React.js and Firebase.",
+    tags: ["React.js", "Firebase"],
+
+    demo: "https://firegram-react-aa8fa.web.app/",
+    github: "https://github.com/anushpoudel/firegram-react",
+  },
+  {
+    id: 2,
+    name: "Guess the song",
+    description:
+      "A simple guess the song game based on the artist. It is built using JavaScript and some jQuery.",
+    tags: ["JavaScript", "jQuery", "OOP"],
+
+    demo: "https://anushpoudel.github.io/oop-game-app-show/",
+    github: "https://github.com/anushpoudel/oop-game-app-show",
+  },
+  {
+    id: 3,
+    name: "My blogs application",
+    description: "A simple CRUD blog application built on top of Next.js",
+    tags: ["Next.js", "React.js"],
+
+    demo: "https://anushblogs.netlify.app/",
+    github: "https://github.com/anushpoudel/anush-blogs-react",
+  },
+  {
+    id: 4,
+    name: "Netflix Clone",
+    description:
+      "A Netflix clone built using ReactJS. With realtime fetching of movies from using themoviedb API.",
+    tags: ["React.js", "API"],
+
+    demo: "https://netflix-clone-f32b0.web.app/",
+    github: "https://github.com/anushpoudel/netflix-clone-react",
+  },
+];
